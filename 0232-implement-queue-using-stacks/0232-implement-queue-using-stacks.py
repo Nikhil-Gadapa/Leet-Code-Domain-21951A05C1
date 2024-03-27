af14e74639d60dc457719bc29fdb6bf97249e409
Nikhil-Gadapa <122527127+Nikhil-Gadapa@users.noreply.github.com>
2024-03-27 10:47:26 +0530

    Time: 36 ms (47.25%), Space: 16.5 MB (69.23%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,27 +1,15 @@
 class MyQueue:
-
     def __init__(self):
-        self.push_stack = []
-        self.pop_stack = []
-
-    def push(self, x: int) -> None:
-        self.push_stack.append(x)
-
-    def pop(self) -> int:
-        if self.empty():
-            return -1  
-        if len(self.pop_stack) == 0:
-            while len(self.push_stack) > 0:
-                self.pop_stack.append(self.push_stack.pop())
-        return self.pop_stack.pop()
-
-    def peek(self) -> int:
-        if self.empty():
-            return -1  
-        if len(self.pop_stack) == 0:
-            while len(self.push_stack) > 0:
-                self.pop_stack.append(self.push_stack.pop())
-        return self.pop_stack[-1]
-
-    def empty(self) -> bool:
-        return len(self.push_stack) == 0 and len(self.pop_stack) == 0
+        self.stack=[]
+        
+    def push(self,x:int)->None:
+        self.stack.append(x)
+        
+    def pop(self)->int:
+        return self.stack.pop(0)
+    
+    def peek(self):
+        return self.stack[0]
+    def empty(self):
+        return len(self.stack)==0
+    
