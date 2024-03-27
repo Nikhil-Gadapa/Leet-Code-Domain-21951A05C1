cee2ccf6b098978aa92712fa888058b7bbc2e584
Nikhil-Gadapa <122527127+Nikhil-Gadapa@users.noreply.github.com>
2024-03-27 10:16:15 +0530

    Time: 56 ms (51.72%), Space: 20.2 MB (78.75%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,39 +1,20 @@
-import sys
 class MinStack:
-
     def __init__(self):
-        self.stack = []
-        self.min = sys.maxsize
+        self.mainStack=[]
+        self.minStack=[]
+    def push(self,val:int)->None:
+        self.mainStack.append(val)
         
-
-    def push(self, val: int) -> None:
-        self.min = min(self.min, val)
-        self.stack.append([val, self.min])
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+            
+    def pop(self)->None:
+        popped=self.mainStack.pop()
         
-
-    def pop(self) -> None:
-        self.stack.pop()
-        if(self.stack):
-            self.min = self.stack[-1][1]
-        else:
-            self.min = sys.maxsize
-        
-
-    def top(self) -> int:
-        return self.stack[-1][0]
-        
-
-    def getMin(self) -> int:
-        if self.stack:
-            return self.stack[-1][-1]
-        else:
-            return sys.maxsize
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        if popped==self.minStack[-1]:
+            self.minStack.pop()
+            
+    def top(self)->int:
+        return self.mainStack[-1]
+    def getMin(self)->int:
+        return self.minStack[-1]
